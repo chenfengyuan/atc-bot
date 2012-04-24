75f7415a5c53e0b46407891e6f939b61fc50c19d
Fengyuan Chen <cfy1990@gmail.com>
2012-04-24 13:02:10 +0800

    ignore errors in atc-bot.sh

--- atc-bot.sh
+++ atc-bot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #(progn (ql:quickload "alexandria") (ql:quickload "inotify")(ql:quickload "trivial-timeout")(load "/home/cfy/gits/clp/count-time.lisp")(load "/home/cfy/gits/atc-bot/atc-bot.lisp"))
 #(sb-ext:save-lisp-and-die "/tmp/atc-bot" :executable t)
-echo -e '(in-package :cfy.atc-bot)\n(main-func)'|./atc-bot $1
+echo -e '(in-package :cfy.atc-bot)\n(ignore-errors (main-func))(sb-ext:quit)'|./atc-bot $1
